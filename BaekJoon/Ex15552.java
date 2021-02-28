b884ed852dd7f821fc9a113b55e4e3bfdcda8698
hongchii <flyh0321@gmail.com>
2021-02-28 21:08:20 +0900

    빠른 A+B

---
+++ BaekJoon/Ex15552.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Ex15552 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		for (int i = 1; i <= t; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+}
