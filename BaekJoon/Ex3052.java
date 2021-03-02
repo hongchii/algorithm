714ab4609e953e0b3dcae7ab46419fb30f177b03
hongchii <flyh0321@gmail.com>
2021-03-02 23:43:53 +0900

    나머지

---
+++ BaekJoon/Ex3052.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Ex3052 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[10];
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt() % 42;
+		}
+		sc.close();
+
+		for (int i = 0; i < arr.length; i++) {
+			int tmp = 0;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					tmp++;
+				}
+			}
+			if (tmp == 0) {
+				count++;
+			}
+
+		}
+		System.out.println(count);
+
+	}
+
+}
