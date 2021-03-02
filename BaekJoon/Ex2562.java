966abd5b442f40ade966494b6ca18e7792d30e5f
hongchii <flyh0321@gmail.com>
2021-03-02 13:36:35 +0900

    최댓값

---
+++ BaekJoon/Ex2562.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Ex2562 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] arr = new int[9];
+		int max = arr[0];
+		int index = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+				index = i + 1;
+			}
+
+		}
+		System.out.println(max + "\n" + index);
+	}
+
+}
