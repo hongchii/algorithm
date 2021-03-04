84766b8061e98e7c26d958cac14ac74e6b508156
hongchii <flyh0321@gmail.com>
2021-03-04 11:02:06 +0900

    이것이 코딩테스트다 87p 거스름돈

---
+++ Greedy/ExGreedy01.java
@@ -0,0 +1,18 @@
+package Greedy;
+
+public class ExGreedy01 {
+
+	public static void main(String[] args) {
+		int n = 1260;
+		int cnt = 0;
+		int[] coinType = { 500, 100, 50, 10 };
+
+		for (int i = 0; i < coinType.length; i++) {
+			int coin = coinType[i];
+			cnt += n / coin;
+			n %= coin;
+		}
+		System.out.println(cnt);
+	}
+
+}
