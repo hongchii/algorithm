403ce0be7baafe65b4acec341bdf447cfbfcf5b9
hongchii <flyh0321@gmail.com>
2021-03-05 17:36:54 +0900

    숫자의 합

---
+++ BaekJoon/Ex11720.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Ex11720 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		String m = sc.next();
+
+		int tot = 0;
+
+		for (int i = 0; i < n; i++) {
+			tot += Integer.parseInt(m.substring(i, i+1));
+		}
+		System.out.println(tot);
+	}
+}
