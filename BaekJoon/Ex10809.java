b41f43d425bd5e8eb2fb8db040d20fe07ff47c8d
hongchii <flyh0321@gmail.com>
2021-03-06 12:30:41 +0900

    알파벳 찾기

---
+++ BaekJoon/Ex10809.java
@@ -0,0 +1,17 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Ex10809 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String alphabet = sc.next();
+
+		for (char ch = 'a'; ch <= 'z'; ch++) {
+			System.out.print(alphabet.indexOf(ch) + " ");
+		}
+	}
+
+}
