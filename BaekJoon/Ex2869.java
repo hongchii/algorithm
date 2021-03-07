e850bff8b51893beb0e0164ba0102414dd9a7c6a
hongchii <flyh0321@gmail.com>
2021-03-07 20:16:38 +0900

    달팽이는 올라가고 싶다

---
+++ BaekJoon/Ex2869.java
@@ -0,0 +1,26 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Ex2869 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int v = Integer.parseInt(st.nextToken());
+
+		long day = (v - b) / (a - b);
+
+		if ((v - b) % (a - b) != 0) {
+			day++;
+		}
+		System.out.println(day);
+	}
+
+}
