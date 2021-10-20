22125da04160ccbf182a2ba84c21b565608d7827
hongchii <flyh0321@gmail.com>
2021-10-20 11:35:23 +0900

    8393 - 합

---
+++ baekJoon2/Ex8393.java
@@ -0,0 +1,18 @@
+package baekJoon2;
+
+import java.util.Scanner;
+
+public class Ex8393 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for( int i = 1; i <= n; i++) {
+			System.out.println(i + n);
+		}
+	}
+
+}
