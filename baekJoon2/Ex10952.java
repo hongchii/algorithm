2e12310cfecd9c2908189a9820ac37cd7a4cb94d
hongchii <flyh0321@gmail.com>
2021-10-21 14:48:41 +0900

    10952 - A+B-5

---
+++ baekJoon2/Ex10952.java
@@ -0,0 +1,24 @@
+package baekJoon2;
+
+import java.util.Scanner;
+
+public class Ex10952 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		while (true) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			
+			if (a == 0 || b == 0) {
+				break;
+			}else {
+			System.out.println(a+b);
+			}
+			
+		}
+	}
+
+}
