ff1f3d9fd60d2bd2da494f891716175a776414c0
hongchii <flyh0321@gmail.com>
2021-10-23 16:37:00 +0900

    Lv.1 직사각형 별찍기

---
+++ programmers/Level1_Ex01.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Level1_Ex01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
