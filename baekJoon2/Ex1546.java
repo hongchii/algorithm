2faf9b541600cffad0d9b6d859ae9a8caa5a7c31
hongchii <flyh0321@gmail.com>
2021-10-23 12:50:40 +0900

    1546 - 평균

--- baekJoon2/Ex1546.java
+++ baekJoon2/Ex1546.java
@@ -10,21 +10,16 @@ public class Ex1546 {
 		int n = scan.nextInt();
 		int arr[] = new int[n];
 		float total = 0;
-		
-		for(int i = 0; i < n; i++) {
-			arr[i] = scan.nextInt();
-		}
-		
 		float max = arr[0];
+		float tmp = 0;
 		
 		for(int i = 0; i < arr.length; i++) {
-			
+			arr[i] = scan.nextInt();
 			if(max < arr[i]) {
 				max = arr[i];
 			}
 		}
 		
-		float tmp = 0;
 		for(int i = 0; i < arr.length; i++) {
 				tmp = arr[i] / max * 100;
 				total += tmp;
