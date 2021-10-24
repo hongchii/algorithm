92006fcc8bd76d0726e11ddd7755d7a87359dd3d
hongchii <flyh0321@gmail.com>
2021-10-24 11:23:34 +0900

    15596 - 정수 n개의 합

---
+++ baekJoon2/Ex15596.java
@@ -0,0 +1,26 @@
+package baekJoon2;
+
+public class Ex15596 {
+	/*
+	 * Java: long sum(int[] a); (클래스 이름: Test)
+	   a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 
+	   (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+	   리턴값: a에 포함되어 있는 정수 n개의 합
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public long sum(int[] a) {
+		long b = 0;
+		
+		for(int i = 0; i < a.length; i++) {
+			b += a[i];
+		}
+		
+		return b;
+	}
+	
+}
