1956e0b3b1f9ab1b25eb6a3a84e8a682cdd6b569
hongchii <flyh0321@gmail.com>
2021-10-26 14:06:14 +0900

    Lv1. 제일 작은 수 제거하기

---
+++ programmers/Level1_Ex12.java
@@ -0,0 +1,41 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Level1_Ex12 {
+
+	public static void main(String[] args) {
+		// 제일 작은 수 제거하기
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		int arr[] = new int[n];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
+		}
+
+		if (arr.length == 1) {
+			System.out.println(new int[] { -1 });
+		}
+
+		int minindex = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[minindex] > arr[i]) {
+				minindex = i;
+			}
+		}
+		int answer[] = new int[arr.length - 1];
+		int cnt = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (minindex == i) {
+				continue;
+			}
+			answer[cnt++] = arr[i];
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
