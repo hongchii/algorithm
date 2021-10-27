dcf9af4a106e74da4515b999b05312e516647688
hongchii <flyh0321@gmail.com>
2021-10-27 13:12:43 +0900

    1152 - 단어의 개수

---
+++ baekJoon2/Ex1152.java
@@ -0,0 +1,19 @@
+package baekJoon2;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Ex1152 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String str = scan.nextLine();
+		scan.close();
+		
+		StringTokenizer st = new StringTokenizer(str, " ");
+		
+		System.out.println(st.countTokens());
+	}
+
+}
