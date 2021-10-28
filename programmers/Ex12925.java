9d39f4f7dfa8ef36cff83c811abfb98dded3c509
hongchii <flyh0321@gmail.com>
2021-10-28 22:19:43 +0900

    level1 - 문자열을 정수로 바꾸기

---
+++ programmers/Ex12925.java
@@ -0,0 +1,16 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Ex12925 {
+
+	public static void main(String[] args) {
+		// 문자열을 정수로 바꾸기
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		int answer = Integer.parseInt(s);
+
+		System.out.println(answer);
+	}
+
+}
