3f5a5e7879b55faf604f348564be1068d5998654
hongchii <flyh0321@gmail.com>
2021-10-29 17:11:32 +0900

    level1. 문자열 다루기 기본

---
+++ programmers/Ex12918.java
@@ -0,0 +1,33 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Ex12918 {
+
+	public static void main(String[] args) {
+		// 문자열 다루기 기본
+		boolean answer = true;
+		
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		
+		if ( s.length() == 4 || s.length() == 6) {
+			for(char ch = 'a'; ch <= 'z'; ch++) {
+				int a = s.indexOf(ch);
+				
+				if (a > -1) {
+					answer = false;
+					break;
+				}else {
+					answer = true;
+				}
+			}
+			
+		}else {
+			answer = false;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
