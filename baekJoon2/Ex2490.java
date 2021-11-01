bf4f6f57efba4f51f6e23bb916e08fc887c731f3
hongchii <flyh0321@gmail.com>
2021-11-01 18:16:17 +0900

    2490 - 윷놀이

---
+++ baekJoon2/Ex2490.java
@@ -0,0 +1,48 @@
+package baekJoon2;
+
+import java.util.Scanner;
+
+public class Ex2490 {
+
+	public static void main(String[] args) {
+		// 윷놀이
+		/*
+		 * 도는 A, 개는 B, 걸은 C, 윷은 D, 모는 E로 배(0)와 등(1)
+		 */
+
+		Scanner scan = new Scanner(System.in);
+
+		int result[] = new int[4];
+		int cnt = 0;
+
+		for (int i = 0; i < 3; i++) { // 총 3번
+			for (int j = 0; j < result.length; j++) {
+				result[j] = scan.nextInt(); // 윷 던지기
+
+				if (result[j] == 1) { // 1(등)이면
+					cnt++;
+				}
+
+			}
+			switch (cnt) {
+			case 0:
+				System.out.println("D"); // 모
+				break;
+			case 1:
+				System.out.println("C"); // 걸
+				break;
+			case 2:
+				System.out.println("B"); // 개
+				break;
+			case 3:
+				System.out.println("A"); // 도
+				break;
+			case 4:
+				System.out.println("E"); // 윷
+				break;
+			}
+			cnt = 0; // 초기화
+		}
+	}
+
+}
