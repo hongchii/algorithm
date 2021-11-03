e8574fa7e7931e7bd94556e8163b222faf83c954
hongchii <flyh0321@gmail.com>
2021-11-03 14:26:39 +0900

    2747 - 피보나치 수

---
+++ baekJoon2/Ex2747.java
@@ -0,0 +1,27 @@
+package baekJoon2;
+
+import java.util.Scanner;
+
+public class Ex2747 {
+
+	public static void main(String[] args) {
+		// 피보나치 수
+
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int a = 0, b = 1, c = 0;
+
+		if (n == 1) {
+			System.out.println(1);
+		} else {
+			for (int i = 1; i < n; i++) {
+				c = a + b;
+				a = b;
+				b = c;
+
+			}
+			System.out.println(c);
+		}
+	}
+
+}
