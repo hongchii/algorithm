b7b3b874fa891a960c4df739fca418215b7565af
hongchii <flyh0321@gmail.com>
2021-11-05 15:11:38 +0900

    이.코.테 p.37 예제 3-1 거스름돈

---
+++ Greedy/P87.java
@@ -0,0 +1,21 @@
+package Greedy;
+
+public class P87 {
+
+	public static void main(String[] args) {
+		// 이.코.테 예제 3-1 거스름돈
+
+		int n = 1260;
+		int[] coin = { 500, 100, 50, 10 };
+
+		int cnt = 0, tmp = 0;
+
+		for (int i = 0; i < coin.length; i++) {
+			cnt += n / coin[i];
+			n %= coin[i];
+
+		}
+		System.out.println(cnt);
+	}
+
+}
